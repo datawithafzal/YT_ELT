29aa33b6e18eabefed951818fc522eadd30c90d5
datawithafzal <afzal.dat@gmail.com>
2025-02-08 18:08:21 +0100

    Create 1st DAG in main.py, Update .gitignore to include logs dir and also upload new JSON file

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,16 +2,20 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
 
+@task
 def get_playlist_id():
 
     try:
@@ -38,6 +42,7 @@ def get_playlist_id():
         raise e
 
 
+@task
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -76,6 +81,7 @@ def get_video_ids(playlistId):
         raise e
 
 
+@task
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -120,6 +126,7 @@ def extract_video_data(video_ids):
         raise e
 
 
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
