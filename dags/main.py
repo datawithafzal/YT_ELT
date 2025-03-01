31615c3a8d757aac06930842197cd02721f2c13c
datawithafzal <afzal.dat@gmail.com>
2025-03-01 15:07:25 +0100

    Scripts relating to interaction with Postgres DWH using Python. Updated main.py with the DWH Dag

--- dags/main.py
+++ dags/main.py
@@ -8,6 +8,8 @@ from api.video_stats import (
     save_to_json,
 )
 
+from datawarehouse.dwh import staging_table, core_table
+
 # Define the local timezone
 local_tz = pendulum.timezone("Europe/Malta")
 
@@ -42,3 +44,18 @@ with DAG(
 
     # Define dependencies
     playlist_id >> video_ids >> extract_data >> save_to_json_task
+
+with DAG(
+    dag_id="update_db",
+    default_args=default_args,
+    description="DAG to process JSON file and insert data into bioth staging and core schemas",
+    schedule="0 15 * * *",
+    catchup=False,
+) as dag:
+
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+
+    # Define dependencies
+    update_staging >> update_core
